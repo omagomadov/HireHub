8c2d4f5f38d0d6099c85409bb3e381abc23311ad
Oumar Magomadov <54516@etu.he2b.be>
2023-03-11 17:41:39 +0100

    Displaying the 'sandwitch' icon on the screen

--- app/src/main/java/g54516/loginapp/MainActivity.kt
+++ app/src/main/java/g54516/loginapp/MainActivity.kt
@@ -3,6 +3,7 @@ package g54516.loginapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -12,12 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var navigationController : NavController
+    private lateinit var drawer : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navigationController = this.findNavController(R.id.navigation_host)
+        drawer = binding.drawerLayout
         // Displaying navigation drawer (need to swipe left to right)
         NavigationUI.setupWithNavController(binding.navigationView, navigationController)
+        // Displaying the 'sandwich' icon
+        NavigationUI.setupActionBarWithNavController(this, navigationController, drawer)
     }
 }
