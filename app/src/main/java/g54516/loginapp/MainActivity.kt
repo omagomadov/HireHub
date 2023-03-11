88dac81c16f3f3b49d9f647f093ec6b2adb1221a
Oumar Magomadov <54516@etu.he2b.be>
2023-03-11 17:38:25 +0100

    Displaying navigation drawer

--- app/src/main/java/g54516/loginapp/MainActivity.kt
+++ app/src/main/java/g54516/loginapp/MainActivity.kt
@@ -3,14 +3,21 @@ package g54516.loginapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import g54516.loginapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navigationController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navigationController = this.findNavController(R.id.navigation_host)
+        // Displaying navigation drawer (need to swipe left to right)
+        NavigationUI.setupWithNavController(binding.navigationView, navigationController)
     }
 }
