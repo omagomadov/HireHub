54e603f0206816f57bbad7c1dbdb54b17ee7a2c1
Oumar Magomadov <54516@etu.he2b.be>
2023-03-11 17:00:55 +0100

    Set the DataBinding inside MainActivity class

--- app/src/main/java/g54516/loginapp/MainActivity.kt
+++ app/src/main/java/g54516/loginapp/MainActivity.kt
@@ -2,10 +2,15 @@ package g54516.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import g54516.loginapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
