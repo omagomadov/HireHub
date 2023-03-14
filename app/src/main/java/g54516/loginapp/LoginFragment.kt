786913cddf400a1cf5e8cfa8b04b2ad8eeb02d89
Oumar Magomadov <54516@etu.he2b.be>
2023-03-14 14:50:07 +0100

    Update verifyEmail method
    - Display toast either the email is valid or not

--- app/src/main/java/g54516/loginapp/LoginFragment.kt
+++ app/src/main/java/g54516/loginapp/LoginFragment.kt
@@ -1,33 +1,49 @@
 package g54516.loginapp
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import g54516.loginapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    private lateinit var binding : FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var toast: Toast
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil
-            .inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login,
-                container, false)
-        binding.loginButton.setOnClickListener { view : View ->
+            .inflate<FragmentLoginBinding>(
+                inflater, R.layout.fragment_login,
+                container, false
+            )
+        binding.loginButton.setOnClickListener { view: View ->
             verifyEmail(view)
         }
         return binding.root
     }
 
-    private fun verifyEmail(view : View) {
-        //todo
+    private fun verifyEmail(view: View) {
+        // Initialize a toast
+        toast =
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.loginEmail.text).matches()) {
+                Toast.makeText(activity, "Valid email !", Toast.LENGTH_SHORT)
+            } else {
+                Toast.makeText(activity, "Invalid email !", Toast.LENGTH_SHORT)
+            }
+        // Display the toast
+        toast.show()
+        // Hide 'virtual keyboard' after finished
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
