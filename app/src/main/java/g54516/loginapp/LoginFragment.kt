064c6ecb03c44baa7328359798ad354653d44734
Oumar Magomadov <54516@etu.he2b.be>
2023-03-14 12:21:21 +0100

    Set binding & define listener on loginButton

--- app/src/main/java/g54516/loginapp/LoginFragment.kt
+++ app/src/main/java/g54516/loginapp/LoginFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import g54516.loginapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil
+            .inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login,
+                container, false)
+        binding.loginButton.setOnClickListener { view : View ->
+            verifyEmail(view)
+        }
+        return binding.root
+    }
+
+    private fun verifyEmail(view : View) {
+        //todo
     }
 
 }
