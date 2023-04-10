31b89f2f3f8f18890fb890775dcc177dd6e7e3c3
Oumar Magomadov <54516@etu.he2b.be>
2023-04-10 20:22:09 +0200

    Add HireHubDB database

--- app/src/main/java/g54516/hireHub/database/HireHubDB.kt
+++ app/src/main/java/g54516/hireHub/database/HireHubDB.kt
@@ -1,9 +1,39 @@
 package g54516.hireHub.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import g54516.hireHub.database.dao.UserDao
 import g54516.hireHub.database.entity.User
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
-abstract class HireHubDB {
-    //todo
+abstract class HireHubDB : RoomDatabase() {
+
+    abstract val userDao : UserDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE : HireHubDB? = null
+
+        fun getInstance(context : Context) : HireHubDB {
+            synchronized(this) {
+                var instance = INSTANCE
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        HireHubDB::class.java,
+                        "HireHubDB"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+    }
+
 }
