5894c29ef08f26e1e4ad31f99bf278aabf85d766
Oumar Magomadov <54516@etu.he2b.be>
2023-04-11 17:29:42 +0200

    Added displayToast attribute & updated save() method in LoginViewModel

---
+++ app/src/main/java/g54516/hireHub/model/LoginViewModel.kt
@@ -0,0 +1,55 @@
+package g54516.hireHub.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+/**
+ * ViewModel for the LoginFragment.
+ *
+ * @property isEmailValid A LiveData object indicating that an email address is valid or not.
+ * @property displayToast A LiveData object indicating that an toast must be displayed or not.
+ */
+class LoginViewModel : ViewModel() {
+
+    private var _isEmailValid = MutableLiveData<Boolean>()
+    val isEmailValid: LiveData<Boolean>
+        get() = _isEmailValid
+
+    private var _displayToast = MutableLiveData<Boolean>()
+    val displayToast: LiveData<Boolean>
+        get() = _displayToast
+
+    init {
+        _isEmailValid.value = false
+        _displayToast.value = false
+    }
+
+    /**
+     * Saves the given email and date in the database.
+     *
+     * If the email already exists in the database, the date will be updated.
+     * If the email does not exist in the database, a new record with the email and date will be created.
+     *
+     * This method sets the value of [_isEmailValid] to indicate if the email address is valid or not.
+     *
+     * @param date The date to store in the database.
+     * @param email The email address to store in the database.
+     */
+    fun save(date: Date, email: String) {
+        _isEmailValid.value = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    /**
+     * Setter for [displayToast] attribute.
+     *
+     * This attribute controls whether or not toast messages should be displayed.
+     *
+     * @param isOn to set the [displayToast]
+     */
+    fun setDisplayToast(isOn: Boolean) {
+        _displayToast.value = isOn
+    }
+
+}
--- app/src/main/java/g54516/hireHub/ui/fragments/LoginFragment.kt
+++ app/src/main/java/g54516/hireHub/ui/fragments/LoginFragment.kt
@@ -9,12 +9,25 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import g54516.hireHub.model.LoginViewModel
 import g54516.hirehub.R
 import g54516.hirehub.databinding.FragmentLoginBinding
+import java.util.*
 
+/**
+ * Fragment for the login screen.
+ *
+ * @property binding The binding object that inflates
+ * the layout and allows access to the UI components.
+ * @property viewModel The [LoginViewModel] instance associated with this fragment.
+ * @property toast A [Toast] object that displays messages to the user.
+ */
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var viewModel: LoginViewModel
     private lateinit var toast: Toast
 
     override fun onCreateView(
@@ -23,23 +36,38 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil
             .inflate(inflater, R.layout.fragment_login, container, false)
-        binding.loginButton.setOnClickListener { view: View -> verifyEmail(view) }
+        binding.loginButton.setOnClickListener {
+            saveEmail()
+        }
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        viewModel.isEmailValid.observe(viewLifecycleOwner, Observer { isEmailValid ->
+            if (viewModel.displayToast.value == true) {
+                toast = if (isEmailValid) {
+                    Toast.makeText(activity, "Valid email !", Toast.LENGTH_SHORT)
+                } else {
+                    Toast.makeText(activity, "Invalid email !", Toast.LENGTH_SHORT)
+                }
+                toast.show()
+                // Hide 'virtual keyboard' after finished
+                val imm =
+                    activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view?.windowToken, 0)
+            }
+        })
         return binding.root
     }
 
-    private fun verifyEmail(view: View) {
-        // Initialize a toast
-        toast =
-            if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.loginEmail.text).matches()) {
-                Toast.makeText(activity, "Valid email !", Toast.LENGTH_SHORT)
-            } else {
-                Toast.makeText(activity, "Invalid email !", Toast.LENGTH_SHORT)
-            }
-        // Display the toast
-        toast.show()
-        // Hide 'virtual keyboard' after finished
-        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    /**
+     * Saves the user's email address to the database when the 'login' button is clicked.
+     *
+     * This method is called by the click listener associated with the 'login' button in the layout file.
+     * It retrieves the email
+     * address entered by the user and calls the [LoginViewModel.save] method to save it to the database.
+     */
+    private fun saveEmail() {
+        viewModel.setDisplayToast(true)
+        viewModel.save(Date(), binding.loginEmail.text.toString())
+        viewModel.setDisplayToast(false)
     }
 
 }
