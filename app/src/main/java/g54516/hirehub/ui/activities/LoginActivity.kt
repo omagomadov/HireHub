b6e583d4f11636b7206079caf1cedb47c2803241
Oumar Magomadov <54516@etu.he2b.be>
2023-05-11 00:06:15 +0200

    Add SearchFragment & refractor bottom/top bar

--- app/src/main/java/g54516/hirehub/ui/activities/LoginActivity.kt
+++ app/src/main/java/g54516/hirehub/ui/activities/LoginActivity.kt
@@ -1,13 +1,14 @@
 package g54516.hirehub.ui.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import g54516.hirehub.R
 import g54516.hirehub.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,10 +19,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         navController = findNavController(R.id.login_navigation_host)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        setSupportActionBar(binding.topBar)
+        // Using 'elvis' operator to avoid NullPointerException
+        // If SupportActionBar is null -> it is not a problem because code will not be executed
+        supportActionBar?.let { actionBar ->
+            // Display the 'back button' on the top bar
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, binding.loginDrawerLayout)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            // 'android.R.id.home' is the 'back button' of Android
+            // Don't confuse it with the ID in the resources.
+            // android.R.id.home != R.id.home
+            android.R.id.home -> {
+                // 'onBackPressed()' is deprecated
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/g54516/hirehub/ui/activities/MainActivity.kt
+++ app/src/main/java/g54516/hirehub/ui/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import g54516.hirehub.R
 import g54516.hirehub.databinding.ActivityMainBinding
 
@@ -18,10 +17,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navController = findNavController(R.id.main_navigation_host)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        setupBottomBarSelectListener()
+        setupBottomBarReselectListeners()
+        setSupportActionBar(binding.topBar)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, binding.mainDrawerLayout)
+    private fun setupBottomBarSelectListener() {
+        // Listeners when clicked on the corresponding menu in bottom bar
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.search -> {
+                    navController.navigate(R.id.action_homeFragment_to_searchFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setupBottomBarReselectListeners() {
+        // Listeners when 're-clicked' on the corresponding menu in bottom bar
+        // (if not set -> app crash !!)
+        binding.bottomNavigation.setOnItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    true
+                }
+                R.id.search -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
