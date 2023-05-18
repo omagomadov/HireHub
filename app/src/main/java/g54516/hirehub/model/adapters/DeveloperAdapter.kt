0d78823b67d2621ad3b2e50191f4a7c4a091bffa
Oumar Magomadov <54516@etu.he2b.be>
2023-05-18 18:20:25 +0200

    - Use Glide instead of Picasso for images

--- app/src/main/java/g54516/hirehub/model/adapters/DeveloperAdapter.kt
+++ app/src/main/java/g54516/hirehub/model/adapters/DeveloperAdapter.kt
@@ -1,12 +1,8 @@
 package g54516.hirehub.model.adapters
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import g54516.hirehub.R
 import g54516.hirehub.database.dto.DeveloperDto
-import g54516.hirehub.databinding.DeveloperCardBinding
 import g54516.hirehub.model.viewholders.DeveloperViewHolder
 
 class DeveloperAdapter() :
@@ -19,10 +15,7 @@ class DeveloperAdapter() :
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeveloperViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding: DeveloperCardBinding =
-            DataBindingUtil.inflate(inflater, R.layout.developer_card, parent, false)
-        return DeveloperViewHolder(binding)
+        return DeveloperViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/g54516/hirehub/model/viewholders/DeveloperViewHolder.kt
+++ app/src/main/java/g54516/hirehub/model/viewholders/DeveloperViewHolder.kt
@@ -1,13 +1,28 @@
 package g54516.hirehub.model.viewholders
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.storage.FirebaseStorage
-import com.squareup.picasso.Picasso
+import g54516.hirehub.R
 import g54516.hirehub.database.dto.DeveloperDto
 import g54516.hirehub.databinding.DeveloperCardBinding
 
-class DeveloperViewHolder(private val binding: DeveloperCardBinding) :
+class DeveloperViewHolder private constructor(private val binding: DeveloperCardBinding) :
     RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        fun from(parent: ViewGroup): DeveloperViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding: DeveloperCardBinding =
+                DataBindingUtil
+                    .inflate(inflater, R.layout.developer_card, parent, false)
+            return DeveloperViewHolder(binding)
+        }
+    }
 
     fun bind(developer: DeveloperDto) {
 
@@ -15,7 +30,13 @@ class DeveloperViewHolder(private val binding: DeveloperCardBinding) :
         val ref = instance.child(developer.avatar)
 
         ref.downloadUrl.addOnSuccessListener { uri ->
-            Picasso.get().load(uri.toString()).into(binding.avatarDeveloper)
+            Glide
+                .with(binding.root.context)
+                .load(uri.toString())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .onlyRetrieveFromCache(true)
+                .apply(RequestOptions.circleCropTransform())
+                .into(binding.avatarDeveloper)
         }
 
         binding.developer = developer
