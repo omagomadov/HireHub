26b82f0ca7bcbf3c3dc703070b7c4c6450f4e344
Oumar Magomadov <54516@etu.he2b.be>
2023-05-21 00:20:16 +0200

    Surround Firestore query inside try..catch (good practice)

--- app/src/main/java/g54516/hirehub/database/repository/AppointmentRepository.kt
+++ app/src/main/java/g54516/hirehub/database/repository/AppointmentRepository.kt
@@ -1,5 +1,7 @@
 package g54516.hirehub.database.repository
 
+import android.content.ContentValues
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import g54516.hirehub.database.dto.AppointmentDto
@@ -11,61 +13,82 @@ import java.time.ZoneOffset
 class AppointmentRepository {
 
     fun addAppointment(user_email: String, developer: DeveloperDto, date: LocalDate) {
-        Firebase.firestore.collection("Appointment")
-            .add(
-                hashMapOf(
-                    "user_email" to user_email,
-                    "developer_email" to developer.email,
-                    "developer_full_name" to developer.firstName + " " + developer.lastName,
-                    "developer_avatar" to developer.avatar,
-                    "date" to date.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
+        try {
+            Firebase.firestore.collection("Appointment")
+                .add(
+                    hashMapOf(
+                        "user_email" to user_email,
+                        "developer_email" to developer.email,
+                        "developer_full_name" to developer.firstName + " " + developer.lastName,
+                        "developer_avatar" to developer.avatar,
+                        "date" to date.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
+                    )
                 )
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de l'insertion d'un rendez-vous:  ${e.toString()}"
             )
+        }
     }
 
     suspend fun getPassedAppointments(user_email: String?): List<AppointmentDto> {
         var appointments = mutableListOf<AppointmentDto>()
-        Firebase.firestore
-            .collection("Appointment")
-            .whereEqualTo("user_email", user_email)
-            .whereLessThan(
-                "date", LocalDate.now().atStartOfDay()
-                    .toInstant(ZoneOffset.UTC).toEpochMilli()
-            )
-            .get()
-            .addOnCompleteListener { snapshot ->
-                if (snapshot.isSuccessful) {
-                    for (document in snapshot.result.documents) {
-                        var appointment = document.toObject(AppointmentDto::class.java)
-                        if (appointment != null) {
-                            appointments.add(appointment)
+        try {
+            Firebase.firestore
+                .collection("Appointment")
+                .whereEqualTo("user_email", user_email)
+                .whereLessThan(
+                    "date", LocalDate.now().atStartOfDay()
+                        .toInstant(ZoneOffset.UTC).toEpochMilli()
+                )
+                .get()
+                .addOnCompleteListener { snapshot ->
+                    if (snapshot.isSuccessful) {
+                        for (document in snapshot.result.documents) {
+                            var appointment = document.toObject(AppointmentDto::class.java)
+                            if (appointment != null) {
+                                appointments.add(appointment)
+                            }
                         }
                     }
-                }
-            }.await()
+                }.await()
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de la récupération des rendez-vous à venir:  ${e.toString()}"
+            )
+        }
         return appointments
     }
 
     suspend fun getIncomeAppointments(user_email: String?): List<AppointmentDto> {
         var appointments = mutableListOf<AppointmentDto>()
-        Firebase.firestore
-            .collection("Appointment")
-            .whereEqualTo("user_email", user_email)
-            .whereGreaterThan(
-                "date", LocalDate.now().atStartOfDay()
-                    .toInstant(ZoneOffset.UTC).toEpochMilli()
-            )
-            .get()
-            .addOnCompleteListener { snapshot ->
-                if (snapshot.isSuccessful) {
-                    for (document in snapshot.result.documents) {
-                        var appointment = document.toObject(AppointmentDto::class.java)
-                        if (appointment != null) {
-                            appointments.add(appointment)
+        try {
+            Firebase.firestore
+                .collection("Appointment")
+                .whereEqualTo("user_email", user_email)
+                .whereGreaterThan(
+                    "date", LocalDate.now().atStartOfDay()
+                        .toInstant(ZoneOffset.UTC).toEpochMilli()
+                )
+                .get()
+                .addOnCompleteListener { snapshot ->
+                    if (snapshot.isSuccessful) {
+                        for (document in snapshot.result.documents) {
+                            var appointment = document.toObject(AppointmentDto::class.java)
+                            if (appointment != null) {
+                                appointments.add(appointment)
+                            }
                         }
                     }
-                }
-            }.await()
+                }.await()
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de la récupération des rendez-vous passés : ${e.toString()}"
+            )
+        }
         return appointments
     }
 
--- app/src/main/java/g54516/hirehub/database/repository/UserRepository.kt
+++ app/src/main/java/g54516/hirehub/database/repository/UserRepository.kt
@@ -1,5 +1,7 @@
 package g54516.hirehub.database.repository
 
+import android.content.ContentValues
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import g54516.hirehub.database.dto.UserDto
@@ -7,30 +9,44 @@ import kotlinx.coroutines.tasks.await
 
 class UserRepository {
 
-    suspend fun getUserByEmail(email: String): UserDto? {
-        var user: UserDto? = null
-        Firebase.firestore
-            .collection("User")
-            .document(email)
-            .get()
-            .addOnSuccessListener { result ->
-                user = result.toObject(UserDto::class.java)
-            }.await()
-        return user
+    fun add(entity: UserDto) {
+        try {
+            Firebase.firestore.collection("User")
+                .document(entity.email)
+                .set(
+                    hashMapOf(
+                        "email" to entity.email,
+                        "firstName" to entity.firstName,
+                        "lastName" to entity.lastName,
+                        "gender" to entity.gender,
+                        "phoneNumber" to entity.phoneNumber
+                    )
+                )
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de l'insertion d'un utilisateur: ${e.toString()}"
+            )
+        }
     }
 
-    fun add(entity: UserDto) {
-        Firebase.firestore.collection("User")
-            .document(entity.email)
-            .set(
-                hashMapOf(
-                    "email" to entity.email,
-                    "firstName" to entity.firstName,
-                    "lastName" to entity.lastName,
-                    "gender" to entity.gender,
-                    "phoneNumber" to entity.phoneNumber
-                )
+    suspend fun getUserByEmail(email: String): UserDto? {
+        var user: UserDto? = null
+        try {
+            Firebase.firestore
+                .collection("User")
+                .document(email)
+                .get()
+                .addOnSuccessListener { result ->
+                    user = result.toObject(UserDto::class.java)
+                }.await()
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de la récupération d'un utilisateur: ${e.toString()}"
             )
+        }
+        return user
     }
 
 }
