78f2885d63130186ed3507804081e78f002c1c6d
Oumar Magomadov <54516@etu.he2b.be>
2023-05-24 02:24:19 +0200

    Update DeveloperRepository

--- app/src/main/java/g54516/hirehub/database/repository/DeveloperRepository.kt
+++ app/src/main/java/g54516/hirehub/database/repository/DeveloperRepository.kt
@@ -1,11 +1,13 @@
 package g54516.hirehub.database.repository
 
+import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.util.Log
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import g54516.hirehub.database.dto.DeveloperDto
+import g54516.hirehub.database.dto.UserDto
 import kotlinx.coroutines.tasks.await
 
 class DeveloperRepository {
@@ -46,6 +48,25 @@ class DeveloperRepository {
             Log.d(TAG, "Erreur lors de la récupération des développeurs ${e.toString()}")
         }
         return developers
+    }
+
+    suspend fun getDeveloperByEmail(email: String): DeveloperDto? {
+        var developer : DeveloperDto? = null
+        try {
+            Firebase.firestore
+                .collection("Developer")
+                .document(email)
+                .get()
+                .addOnSuccessListener { result ->
+                    developer = result.toObject(DeveloperDto::class.java)
+                }.await()
+        } catch (e: Exception) {
+            Log.d(
+                ContentValues.TAG,
+                "Erreur lors de la récupération d'un développeur: ${e.toString()}"
+            )
+        }
+        return developer
     }
 
 }
